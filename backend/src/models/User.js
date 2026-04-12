f4b49343185174c7f85406646163fc813c455e19
md-sakib-chowdhury <mdsakibchowdhuryofficial00@gmail.com>
2026-04-12 09:49:18 +0600

    6th

---
+++ backend/src/models/User.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    phone: { type: String, required: true, unique: true },
+    email: { type: String },
+    password: { type: String, required: true },
+    role: { type: String, enum: ["patient", "admin"], default: "patient" },
+}, { timestamps: true });
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+export default mongoose.model("User", userSchema);
