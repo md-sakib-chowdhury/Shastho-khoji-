8e60f276ffe1ec324cf265977b9995d3e4a7dafa
md-sakib-chowdhury <mdsakibchowdhuryofficial00@gmail.com>
2026-04-15 08:54:27 +0600

    22

---
+++ frontend/src/hooks/useDoctors.js
@@ -0,0 +1,9 @@
+import { useQuery } from "@tanstack/react-query";
+import { getDoctorsApi } from "../api/doctorApi";
+
+export const useDoctors = (params) => {
+    return useQuery({
+        queryKey: ["doctors", params],
+        queryFn: () => getDoctorsApi(params).then((r) => r.data),
+    });
+};
