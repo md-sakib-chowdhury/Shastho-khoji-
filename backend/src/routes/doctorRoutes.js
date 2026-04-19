67cb7e88c7fc1219ea5d3408d600e9ca43d136f3
md-sakib-chowdhury <mdsakibchowdhuryofficial00@gmail.com>
2026-04-19 09:40:22 +0600

    83

--- backend/src/routes/doctorRoutes.js
+++ backend/src/routes/doctorRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { getDoctors, getDoctorById, createDoctor } from "../controllers/doctorController.js";
+import { getDoctors, getDoctorById, createDoctor, updateDoctor, deleteDoctor } from "../controllers/doctorController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 router.get("/", getDoctors);
 router.get("/:id", getDoctorById);
 router.post("/", protect, createDoctor);
+router.put("/:id", protect, updateDoctor);
+router.delete("/:id", protect, deleteDoctor);
 
 export default router;
